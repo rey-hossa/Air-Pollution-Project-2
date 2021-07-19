974c7aece644364427ff97ef13640a11eeaafe6c
rey-hossa <78262774+rey-hossa@users.noreply.github.com>
2021-07-19 22:21:52 +0200

    Add netlify

--- src/index_dev.js
+++ src/index_dev.js
@@ -7,8 +7,40 @@ wallpaper.id = "wallpaper";
 wallpaper.src = "../images/wallpaper.jpg";
 document.body.appendChild(wallpaper);
 */
-
+/*
 const hello = document.createElement('div');
 hello.innerHTML = 'Hello Dev';
 hello.classList.add('hello');
 document.body.appendChild(hello);
+*/
+
+async function aqiChecker(){
+  try{
+
+    let city = document.getElementById("search").value;
+    let uri = "https://api.waqi.info/feed/" + city + "/?token=" + apiKey;
+
+    const res = await fetch(uri);
+    const resJson = await res.json();
+    const aqi = resJson.data.aqi;
+    //console.log(aqi);
+    index.innerHTML = "AQI: " + aqi ;
+
+  } catch (err){
+    console.error(err.message);
+    index.innerHTML = "The city you entered doesn't exist!" ;
+  }
+}
+
+
+
+const apiKey = "d3fdee6489352a6599a5e2d8557718dcd34dbce7";
+
+let enter = document.getElementById('enter');
+let search = document.getElementById('search');
+let index = document.getElementById("index");
+
+enter.addEventListener("click", aqiChecker );
+search.addEventListener("keyup", function(input){
+  if (event.keyCode === 13) {enter.click();}
+});
