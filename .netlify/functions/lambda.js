6d1d86d273240605d824c9f19747d838ac9dfe50
rey-hossa <78262774+rey-hossa@users.noreply.github.com>
2021-07-19 22:34:22 +0200

    lambda.js

--- .netlify/functions/lambda.js
+++ .netlify/functions/lambda.js
@@ -1,12 +1,13 @@
 exports.handler = async event => {
-  // Più tardi imposteremo una variabile d'ambiente interna a Netlify stesso, accessibile semplicemente così:
+  console.log("Entrato nel file");
+
   const API_KEY =  process.env.API_KEY;
 
-  // qui facciamo la chiamata alla API esattamente come la facevamo prima in index_dev.js
+  console.log(API_KEY);
+
   const response = await fetch(`endpoint/parameters&API_KEY=${API_KEY}`);
   const data = await response.json();
 
-  // da qui in giù la funzione fa da back-end: elaboriamo dei dati e li rimandiamo al front-end in formato JSON con uno statusCode 200, cioè "successo".
   const pass = (body) => {
     return {
       statusCode: 200,
