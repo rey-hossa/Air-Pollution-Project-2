1a515afeec2e4141ff0117433dd47b1890783128
rey-hossa <78262774+rey-hossa@users.noreply.github.com>
2021-07-19 20:27:01 +0200

    move image

--- src/index_dev.js
+++ src/index_dev.js
@@ -1,12 +1,12 @@
 
 import './style.css';
 
-
+/*
 const wallpaper = document.createElement('img');
 wallpaper.id = "wallpaper";
 wallpaper.src = "../images/wallpaper.jpg";
 document.body.appendChild(wallpaper);
-
+*/
 
 const hello = document.createElement('div');
 hello.innerHTML = 'Hello WebPack';
