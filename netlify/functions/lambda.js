2e422a68bce9b589e342310951af33fdd68eaa19
rey-hossa <78262774+rey-hossa@users.noreply.github.com>
2021-07-22 17:06:07 +0200

    Update lambda.js

--- netlify/functions/lambda.js
+++ netlify/functions/lambda.js
@@ -3,11 +3,12 @@ exports.handler = async function (event, context) {
   const API_KEY =  process.env.API_KEY;
   const city = event.queryStringParameters.city;
 
-  let uri = "https://api.waqi.info/feed/" + city + "/?token=" + API_KEY;
 
   async function aqi(){
+    let uri = "https://api.waqi.info/feed/" + city + "/?token=" + API_KEY;
     const res = await fetch(uri);
     const resJson = await res.json();
+    return reJson;
   }
   let data = aqi();
 
