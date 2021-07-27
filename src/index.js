bbdd1295283b4a1bfe496a8783c79e302b6398ef
rey-hossa <78262774+rey-hossa@users.noreply.github.com>
2021-07-27 17:24:44 +0200

    add map

--- src/index.js
+++ src/index.js
@@ -43,8 +43,41 @@ async function aqiChecker() {
 let enter = document.getElementById('enter');
 let search = document.getElementById('search');
 let index = document.getElementById("index");
+let status = document.getElementById('status');
+let info = document.getElementById('info');
+let information = document.getElementById('information');
+let map_section = document.getElementById('map');
+
+let heathImplications = document.getElementById('heathImplications');
+let cautionaryStatement = document.getElementById('cautionaryStatement');
+
 
 enter.addEventListener("click", aqiChecker );
 search.addEventListener("keyup", function(input){
   if (event.keyCode === 13) {enter.click();}
 });
+
+//Information section animation
+info.addEventListener("mouseover",function(){
+  map_section.style.display = "none";
+  information.style.display = "flex";
+});
+info.addEventListener("mouseout",function(){
+  map_section.style.display = "flex";
+  information.style.display = "none";
+});
+
+// Map creation
+
+// Leaflet Map Layer
+let OSM_URL = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+let OSM_ATTRIB = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+let osmLayer = L.tileLayer(OSM_URL, {attribution: OSM_ATTRIB});
+// Waqi Layer
+let WAQI_URL  = "https://tiles.waqi.info/tiles/usepa-aqi/{z}/{x}/{y}.png?token=_TOKEN_ID_";
+let WAQI_ATTR = 'Air Quality Tiles &copy; <a href="http://waqi.info">waqi.info</a>';
+let waqiLayer = L.tileLayer(WAQI_URL, {attribution: WAQI_ATTR});
+
+let map = L.map('map').setView([44.500017,11.328501], 11); // Map view coordinates
+map.addLayer(osmLayer);
+map.addLayer(waqiLayer);
